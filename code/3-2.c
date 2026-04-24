ddd5b4cbfc6a94cb0d33cf97b0573efa4b23e88b
apply2 <67138665+apply2@users.noreply.github.com>
2026-04-24 12:03:20 +0900

    Update 3-2.c

---
+++ code/3-2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+   
+    for (int i = argc - 1; i > 0; i--)
+    {
+        for (int j = 0; j < argc - 1 - (argc-2); j++)
+        {
+            if (strcmp(argv[j], argv[j+1]) > 0)
+            {
+                char *p = argv[j];
+                argv[j] = argv[j+1];
+                argv[j+1] = p;
+            }
+            
+        }
+        
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+    
+    return 0;
+}
